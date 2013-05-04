c62db625cce20327d3ee9f64a87c2acf29dd4b6e
Francois Beaune <beaune@aist.enst.fr>
2013-05-04 16:13:57 +0200

    set translucency on vegetation, replace mesh files extensions (.obj -> .binarymesh).

--- mescaline_postexport.py
+++ mescaline_postexport.py
@@ -36,7 +36,7 @@ def load_project_file(filepath):
     try:
         tree.parse(filepath)
     except IOError:
-        print("Failed to load project file {0}.".format(filepath))
+        print("ERROR: failed to load project file {0}.".format(filepath))
         sys.exit(1)
 
     return tree
@@ -45,7 +45,7 @@ def write_project_file(filepath, tree):
     try:
         tree.write(filepath)
     except IOError:
-        print("Failed to write project file {0}.".format(filepath))
+        print("ERROR: failed to write project file {0}.".format(filepath))
         sys.exit(1)
 
 
@@ -80,7 +80,10 @@ def find_material(root, name):
 def find_bsdf(root, name):
     return find_entity(root, 'bsdf', name)
 
-def set_material_fresnel(root, material_marker, new_fresnel):
+def find_surface_shader(root, name):
+    return find_entity(root, 'surface_shader', name)
+
+def set_material_fresnel(root, material_marker, fresnel):
     bsdfs = set()
 
     for material in root.iter('material'):
@@ -91,46 +94,55 @@ def set_material_fresnel(root, material_marker, new_fresnel):
 
     for mix_bsdf in bsdfs:
         microfacet_bsdf = find_bsdf(root, get_param(mix_bsdf, "bsdf1"))
-        old_fresnel_multiplier = get_param(microfacet_bsdf, "fresnel_multiplier")
-        print("Replacing Fresnel multiplier \"{0}\" by \"{1}\" in BSDF \"{2}\"...".format(old_fresnel_multiplier, new_fresnel, microfacet_bsdf.attrib['name']))
-        set_param(microfacet_bsdf, "fresnel_multiplier", new_fresnel)
+        print("  Setting Fresnel multiplier to \"{0}\" on BSDF \"{1}\"...".format(fresnel, microfacet_bsdf.attrib['name']))
+        set_param(microfacet_bsdf, "fresnel_multiplier", fresnel)
+
+def set_material_translucency(root, material_marker, translucency):
+    surface_shaders = set()
+
+    for material in root.iter('material'):
+        if material_marker in material.attrib['name']:
+            surface_shader = find_surface_shader(root, get_param(material, "surface_shader"))
+            assert surface_shader.attrib['model'] == 'physical_surface_shader'
+            surface_shaders.add(surface_shader)
+
+    for surface_shader in surface_shaders:
+        print("  Setting translucency to \"{0}\" on surface shader \"{1}\"...".format(translucency, surface_shader.attrib['name']))
+        set_param(surface_shader, "translucency", translucency)
 
 
 #--------------------------------------------------------------------------------------------------
-# Assign light-emitting entities (EDFs, lights) to separate render layers.
+# Replace mesh file extensions (from .obj to .binarymesh).
 #--------------------------------------------------------------------------------------------------
 
-def assign_render_layers(nodes):
-    for node in nodes:
-        name = node.attrib['name']
-        if node.find("parameter[@name='render_layer']") is None:
-            print("Assigning entity \"{0}\" to render layer \"{0}\"...".format(name))
-            set_param(node, 'render_layer', name)
-        else:
-            print("Entity \"{0}\" is already assigned to a render layer.".format(name))
+def replace_mesh_file_extension(param):
+    if os.path.splitext(param.attrib['value'])[1].lower() != '.obj':
+        return 0
 
+    param.attrib['value'] = os.path.splitext(param.attrib['value'])[0] + '.binarymesh'
 
-#--------------------------------------------------------------------------------------------------
-# Tweak the robe shader.
-#--------------------------------------------------------------------------------------------------
+    return 1
 
-def tweak_robe_shader(root):
-    set_material_fresnel(root, "hood_robe", "0.3")
+def replace_mesh_file_extensions(root):
+    found = 0
 
+    for object in root.iter('object'):
+        if object.attrib['model'] != 'mesh_object':
+            continue
 
-#--------------------------------------------------------------------------------------------------
-# Tweak trees shaders.
-#--------------------------------------------------------------------------------------------------
+        for parameter in object.findall('parameter'):
+            if parameter.attrib['name'] == 'filename':
+                found += replace_mesh_file_extension(parameter)
 
-def tweak_trees_shaders(root):
-    set_material_fresnel(root, "big_branches_", "0.1")
-    set_material_fresnel(root, "big_leaves_",   "0.1")
-    set_material_fresnel(root, "big_leaves_",   "0.1")
-    set_material_fresnel(root, "plant_",        "0.1")
+        for parameters in object.iter('parameters'):
+            for parameter in parameters.findall('parameter'):
+                found += replace_mesh_file_extension(parameter)
+
+    print("  Replaced mesh file extension on {0} file paths.".format(found))
 
 
 #--------------------------------------------------------------------------------------------------
-# Tweak the hair shader.
+# Replace the hair shader.
 #--------------------------------------------------------------------------------------------------
 
 HAIR_MATERIAL_MARKER = "hood_hair"
@@ -138,12 +150,12 @@ NEW_ROOT_HAIR_BRDF_NAME = "hood_hair_brdf"
 
 def fixup_hair_material(assembly, material):
     old_bsdf_name = get_param(material, 'bsdf')
-    print("Replacing BSDF \"{0}\" by BSDF \"{1}\" in material \"{2}\"...".format(old_bsdf_name, NEW_ROOT_HAIR_BRDF_NAME, material.attrib['name']))
+    print("  Replacing BSDF \"{0}\" by BSDF \"{1}\" in material \"{2}\"...".format(old_bsdf_name, NEW_ROOT_HAIR_BRDF_NAME, material.attrib['name']))
     set_param(material, 'bsdf', NEW_ROOT_HAIR_BRDF_NAME)
     return old_bsdf_name
 
 def add_hair_bsdf_network(assembly, reflectance_name):
-    print("Adding BSDF \"{0}\" with reflectance \"{1}\" to assembly \"{2}\"...".format(NEW_ROOT_HAIR_BRDF_NAME, reflectance_name, assembly.attrib['name']))
+    print("  Adding BSDF \"{0}\" with reflectance \"{1}\" to assembly \"{2}\"...".format(NEW_ROOT_HAIR_BRDF_NAME, reflectance_name, assembly.attrib['name']))
     hair_brdf = xml.Element('bsdf')
     hair_brdf.attrib['name'] = NEW_ROOT_HAIR_BRDF_NAME
     hair_brdf.attrib['model'] = "microfacet_brdf"
@@ -174,14 +186,52 @@ def replace_hair_shader(root):
 
 
 #--------------------------------------------------------------------------------------------------
+# Tweak the robe shader.
+#--------------------------------------------------------------------------------------------------
+
+def tweak_robe_shader(root):
+    set_material_fresnel(root, "hood_robe", "0.3")
+
+
+#--------------------------------------------------------------------------------------------------
+# Tweak vegetation shaders.
+#--------------------------------------------------------------------------------------------------
+
+VEGETATION_MATERIAL_MARKERS = [ "big_branches_", "big_leaves_", "plant_" ]
+
+def tweak_vegetation_shaders(root):
+    for material_marker in VEGETATION_MATERIAL_MARKERS:
+        set_material_fresnel(root, material_marker, "0.1")
+        set_material_translucency(root, material_marker, "0.5")
+
+
+#--------------------------------------------------------------------------------------------------
 # Tweak the settings of the frames.
 #--------------------------------------------------------------------------------------------------
 
 def tweak_frames(root):
     for frame in root.iter('frame'):
-        print("Tweaking frame \"{0}\"...".format(frame.attrib['name']))
+        print("  Tweaking frame \"{0}\"...".format(frame.attrib['name']))
         set_param(frame, "filter", "gaussian")
         set_param(frame, "filter_size", "2.5")
+
+
+#--------------------------------------------------------------------------------------------------
+# Assign light-emitting entities (EDFs, lights) to separate render layers.
+#--------------------------------------------------------------------------------------------------
+
+def assign_render_layers_to_nodes(nodes):
+    for node in nodes:
+        name = node.attrib['name']
+        if node.find("parameter[@name='render_layer']") is None:
+            print("  Assigning entity \"{0}\" to render layer \"{0}\"...".format(name))
+            set_param(node, 'render_layer', name)
+        else:
+            print("  Entity \"{0}\" is already assigned to a render layer.".format(name))
+
+def assign_render_layers(root):
+    assign_render_layers_to_nodes([ edf for edf in root.iter('edf') ])
+    assign_render_layers_to_nodes([ light for light in root.iter('light') ])
 
 
 #--------------------------------------------------------------------------------------------------
@@ -189,18 +239,17 @@ def tweak_frames(root):
 #--------------------------------------------------------------------------------------------------
 
 def process_file(filepath):
-    print("Processing {0}...".format(filepath))
+    print("Processing {0}:".format(filepath))
 
     tree = load_project_file(filepath)
     root = tree.getroot()
 
-    assign_render_layers([ edf for edf in root.iter('edf') ])
-    assign_render_layers([ light for light in root.iter('light') ])
-
-    tweak_robe_shader(root)
-    tweak_trees_shaders(root)
+    replace_mesh_file_extensions(root)
     replace_hair_shader(root)
+    tweak_robe_shader(root)
+    tweak_vegetation_shaders(root)
     tweak_frames(root)
+    assign_render_layers(root)
 
     write_project_file(filepath, tree)
 
