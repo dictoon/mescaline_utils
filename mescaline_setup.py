1472cf13ad7d0bfbc563bba0ebac1dc226aa24e6
Francois Beaune <beaune@aist.enst.fr>
2013-05-18 20:57:38 +0200

    fixed a regression in the handling of custom attributes introduced in the previous commit, cleaned up and refactored the code.

--- mescaline_setup.py
+++ mescaline_setup.py
@@ -29,40 +29,41 @@ reload(ms_commands)
 
 CUSTOM_ATTR_NAME = 'UDP3DSMAX'
 
-def parse_custom_attribs(entity):
+def parse_custom_attributes(entity):
     print("Parsing custom attributes on {0}...".format(entity))
 
-    custom_attr_string = cmds.getAttr(entity + '.' + CUSTOM_ATTR_NAME)
-    attrs = dict()
+    attributes = dict()
 
-    for pair in custom_attr_string.split('&cr;&lf;'):
-        if pair:
-            split_pair = pair.split('=')
+    for param in cmds.getAttr(entity + '.' + CUSTOM_ATTR_NAME).split('&cr;&lf;'):
+        if len(param) == 0:
+            continue
 
-            if len(split_pair) != 2:
-                print("Skipping custom attribute string: {0}".format(pair))
-                continue
+        parts = param.split('=')
 
-            key = split_pair[0]
-            attr = split_pair[1]
+        if len(parts) != 2:
+            print("Skipping custom attribute string: {0}".format(param))
+            continue
 
-            if key == 'as_light' or key == 'invisible':
-                attrs[key] = (attr == 'true')
+        key = parts[0].strip()
+        value = parts[1].strip()
 
-            elif key == 'multiplier' or key == 'f_stop':
-                attrs[key] = float(re.match(r'^[0-9\.]+$', attr).group(0))
+        if key == 'as_light' or key == 'invisible':
+            attributes[key] = (value == 'true')
 
-            elif key == 'color':
-                attr_string_list = re.findall(r'[0-9]+', attr)
-                attrs[key] = (float(attr_string_list[0]) / 255, float(attr_string_list[1]) / 255, float(attr_string_list[2]) / 255)
+        elif key == 'multiplier' or key == 'f_stop':
+            attributes[key] = float(re.match(r'^[0-9\.]+$', value).group(0))
 
-            elif key == 'from_spot_light' or key == 'type':
-                attrs[key] = attr
+        elif key == 'color':
+            values = re.findall(r'[0-9]+', value)
+            attributes[key] = (float(values[0]) / 255, float(values[1]) / 255, float(values[2]) / 255)
 
-    return attrs
+        elif key == 'from_spot_light' or key == 'type':
+            attributes[key] = value
+
+    return attributes
 
 
-def convert_area_light(area_light, attrs):
+def convert_area_light(area_light, attributes):
     # create and initialise material for object
     light_material = cmds.createNode('ms_appleseed_material', n=area_light + '_material')
     cmds.setAttr(light_material + '.enable_back_material', 0)
@@ -74,8 +75,8 @@ def convert_area_light(area_light, attrs):
 
     # create and initialise edf
     light_edf = ms_commands.create_shading_node('diffuse_edf', area_light + '_edf')
-    cmds.setAttr(light_edf + '.exitance', attrs['color'][0], attrs['color'][1], attrs['color'][2], type='double3')
-    cmds.setAttr(light_edf + '.exitance_multiplier', attrs['multiplier'], attrs['multiplier'], attrs['multiplier'], type='double3')
+    cmds.setAttr(light_edf + '.exitance', attributes['color'][0], attributes['color'][1], attributes['color'][2], type='double3')
+    cmds.setAttr(light_edf + '.exitance_multiplier', attributes['multiplier'], attributes['multiplier'], attributes['multiplier'], type='double3')
 
     # assign material and connect up nodes
     cmds.select(area_light)
@@ -84,15 +85,15 @@ def convert_area_light(area_light, attrs):
     cmds.connectAttr(light_surface_shader + '.outColor', light_material + '.surface_shader_front_color', f=True)
 
 
-def add_gobo(dummy_object, attrs):
-    attrs = parse_custom_attribs(dummy_object)
-    cmds.select(attrs['from_spot_light'])
+def add_gobo(dummy_object, attributes):
+    attributes = parse_custom_attributes(dummy_object)
+    cmds.select(attributes['from_spot_light'])
 
     shape_node = cmds.listRelatives(dummy_object, shapes=True)[0]
     material = ms_commands.has_shader_connected(shape_node)
     file_node = ms_commands.get_connected_node(material)
 
-    cmds.connectAttr(file_node + '.outColor', attrs['from_spot_light'] + '.color', f=True)
+    cmds.connectAttr(file_node + '.outColor', attributes['from_spot_light'] + '.color', f=True)
     cmds.select(dummy_object)
     cmds.delete()
 
@@ -127,27 +128,17 @@ def setup_dof(target, camera, f_stop):
 
 
 def setup():
-    area_lights = []
-    gobo_dummys = []
-    cameras = []
-
     for transform in cmds.ls(tr=True):
         if cmds.attributeQuery(CUSTOM_ATTR_NAME, n=transform, exists=True):
-            attribs = parse_custom_attribs(transform)
-            if 'type' in attribs.keys():
-                type = attribs['type']
+            attributes = parse_custom_attributes(transform)
+            if 'type' in attributes.keys():
+                type = attributes['type']
                 if type == 'arealight':
-                    area_lights.append([transform, attribs])
+                    convert_area_light(transform, attributes)
                 elif type == 'gobo_dummy':
-                    gobo_dummys.append([transform, attribs])
+                    add_gobo(transform, attributes)
                 elif type == 'camera':
-                    cameras.append([transform, attribs])
-
-    for arealight in area_lights:
-        convert_area_light(arealight[0], arealight[1])
-
-    for gobo_dummy in gobo_dummys:
-        add_gobo(gobo_dummy[0], gobo_dummy[1])
+                    setup_dof('dof_target', transform, attributes['f_stop'])
 
     # adjust light multiplier values
     for light in cmds.ls(lt=True):
@@ -155,8 +146,5 @@ def setup():
         if light_type == 'spotLight' or light_type == 'pointLight':
             intensity = cmds.getAttr(light + '.intensity') * 2.5
             cmds.setAttr(light + '.intensity', intensity)
-
-    for camera in cameras:
-        setup_dof('dof_target', camera[0], camera[1]['f_stop'])
 
     print("Done.")
